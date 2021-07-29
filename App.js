62f683b6c579de816649efe7e8dd89da4d98459d
Josh <joshdefreitas@gmail.com>
2021-07-29 03:19:19 -0400

    basic language picker

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, Picker, View } from "react-native";
 
 export default function App() {
+  const [selectedValue, setSelectedValue] = useState("java");
   return (
     <View style={styles.container}>
-      <Text>Testing changes</Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="English" value="eng" />
+        <Picker.Item label="Spanish" value="span" />
+      </Picker>
       <StatusBar style="auto" />
     </View>
   );

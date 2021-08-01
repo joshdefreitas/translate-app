f525717bc99bd853a24af58360e9e801e7901f78
Josh <joshdefreitas@gmail.com>
2021-07-31 23:58:21 -0400

    converted project to class component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { Text, TextInput, StyleSheet, View } from "react-native";
+import React, { useState, Component } from "react";
+import { Text, TextInput, StyleSheet, View, Button } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 
 const APIKey = require("./APIKey.json");
 const url = require("./url.json");
+
 const langs = [
   {
     label: "English",
@@ -20,34 +21,41 @@ const langs = [
   },
 ];
 
-export default function App() {
-  const [text, setText] = useState("");
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={{ height: 40 }}
-        placeholder="Type here to translate!"
-        onChangeText={(text) => setText(text)}
-        defaultValue={text}
-      />
-      <Text>{"Please select language"}</Text>
-      <RNPickerSelect
-        onValueChange={(value) => console.log(value)}
-        useNativeAndroidPickerStyle={false}
-        style={{
-          inputAndroid: { color: "black" },
-        }}
-        items={langs}
-      />
-      <Text style={{ padding: 10 }}>
-        {text
-          .split(" ")
-          .map((word) => word && "lorem ipsum")
-          .join(" ")}
-      </Text>
-    </View>
-  );
+    this.state = {
+      text: "",
+      tlRes: "",
+    };
+  }
+
+  render() {
+    const { text, tlRes } = this.state;
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({ text })}
+          defaultValue={text}
+        />
+        <Text>{"Please select language"}</Text>
+        <RNPickerSelect
+          onValueChange={(value) => console.log(value)}
+          useNativeAndroidPickerStyle={false}
+          style={{
+            inputAndroid: { color: "black" },
+          }}
+          items={langs}
+        />
+        <Text style={{ padding: 10 }}>{tlRes}</Text>
+
+        <Button title="Translate" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
